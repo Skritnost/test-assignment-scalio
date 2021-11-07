6b243e04318c591cfc430f5b6be69a309042b624
Andrei Komarov <andrei.komarov@clausematch.com>
2021-11-07 17:09:17 +0300

    feat(app): added components to the root and bind api request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { BehaviorSubject, finalize, Observable, Subject, takeUntil } from 'rxjs';
+
+import { UserTableModel } from './components/models/userTableModel';
+import { UsersApiServiceService } from './services/users-api-service.service';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  onFormSubmitted (value: string): void {
+export class AppComponent implements OnDestroy {
+  tableData: UserTableModel[] = [];
 
+  private _isDataLoading: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private _destroy$: Subject<void> = new Subject();
+
+  get isDataLoading$ (): Observable<boolean> {
+    return this._isDataLoading.asObservable();
+  }
+
+  constructor (
+    private _usersApiServiceService: UsersApiServiceService,
+    private _changeDetection: ChangeDetectorRef,
+  ) {
+  }
+
+  ngOnDestroy (): void {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+
+  onFormSubmitted (value: string): void {
+    this._isDataLoading.next(true);
+
+    this._usersApiServiceService.getUsers(value)
+      .pipe(
+        finalize(() => {
+          this._isDataLoading.next(false);
+        }),
+        takeUntil(this._destroy$),
+      )
+      .subscribe((users) => {
+        this.tableData = users;
+
+        this._changeDetection.detectChanges();
+      });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,33 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StringFieldFormComponent } from './components/search-login/string-field-form.component';
+import { SortingPaginationTableComponent } from './components/sorting-pagination-table/sorting-pagination-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StringFieldFormComponent,
+    SortingPaginationTableComponent,
   ],
   imports: [
     ReactiveFormsModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    HttpClientModule,
     FormsModule,
     BrowserModule,
     AppRoutingModule,
