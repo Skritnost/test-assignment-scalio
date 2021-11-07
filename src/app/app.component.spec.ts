610e543a829cd88268751d51093350909d17e6b3
Andrei Komarov <andrei.komarov@clausematch.com>
2021-11-07 17:12:35 +0300

    test(app): added a root component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,54 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, of } from 'rxjs';
 
 import { AppComponent } from './app.component';
+import { UserTableModel } from './components/models/userTableModel';
+import { StringFieldFormComponent } from './components/search-login/string-field-form.component';
+import { SortingPaginationTableComponent } from './components/sorting-pagination-table/sorting-pagination-table.component';
+import { UsersApiServiceService } from './services/users-api-service.service';
+
+const USERS_MOCK: UserTableModel[] = [{
+  id: { value: 1, label: 'Id' },
+  login: { value: 'One', label: 'Login' },
+  avatarUrl: { value: 'url', label: 'Avatar url' },
+  type: { value: 'User', label: 'Type' },
+}];
+
+class UsersApiServiceServiceMock {
+  getUsers (value: string): Observable<UserTableModel[]> {
+    return of(USERS_MOCK);
+  }
+}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        HttpClientModule,
+      ],
+      providers: [
+        {
+          provide: UsersApiServiceService,
+          useClass: UsersApiServiceServiceMock,
+        },
       ],
       declarations: [
+        StringFieldFormComponent,
+        SortingPaginationTableComponent,
         AppComponent,
       ],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('onFormSubmitted returns users', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.componentInstance;
+
+    component.onFormSubmitted('user');
+
+    expect(component.tableData).toEqual(USERS_MOCK);
+    // @ts-ignore
+    expect(component._isDataLoading.value).toEqual(false);
   });
 });
